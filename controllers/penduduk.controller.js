3f15d6bceb474a9a447345cf412409b36ae62c24
Aizar Rahima <aizarrahima@gmail.com>
2022-02-21 09:43:44 +0700

    update

--- controllers/penduduk.controller.js
+++ controllers/penduduk.controller.js
@@ -1,6 +1,16 @@
 'use strict' // agar tidak memakan banyak memori
 
 const db = require('../db')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const secret = '#$@^%$^%*&%$$@&'
+
+function hashPassword(password) {
+    const salt = bcrypt.genSaltSync(10)
+    return bcrypt.hashSync(password, salt)
+}
+
 
 module.exports = {
     getAll: (req, res) => {
@@ -77,5 +87,66 @@ module.exports = {
                 })
             }
         })
-    }
+    },
+    registrasi: (req, res) => {
+            const {
+                nama_lengkap,
+                email,
+                password
+            } = req.body
+            if (!nama_lengkap, !email || !password) {
+                res.status(402).json({
+                    message: "nama lengkap, email dan password harus diisi!"
+                })
+            }
+            return db.query('insert into pengguna set ?', {
+                nama_lengkap,
+                email,
+                password: hashPassword(password)
+            }, (err, result) => {
+                if (err) {
+                    return res.status(500).json({
+                        err
+                    })
+                }
+                return res.json({
+                    message: 'registrasi berhasil',
+                    data: result
+                })
+            })
+        },
+        login: (req, res) => {
+            const {
+                email,
+                password
+            } = req.body
+            if (!email || !password) res.status(402).json({
+                message: 'email dan password harus diisi.'
+            })
+            return db.query('select * from pengguna where email = ?', email, (err, result) => {
+                if (err) {
+                    return req.status(500).json({
+                        err
+                    })
+                }
+                const user = result[0]
+                if (typeof user === 'undefined') {
+                    return res.status(401).json({
+                        message: 'user tidak ditemukan'
+                    })
+                }
+                if (!bcrypt.compareSync(password, user.password)) {
+                    return res.status(401).json({
+                        message: 'email dan password tidak sesuai'
+                    })
+                }
+                const token = jwt.sign({
+                    data: user
+                }, secret)
+                return res.json({
+                    message: 'Login berhasil. Silahkan menggunakan token dibawah ini untuk mengakses endpoint private lain',
+                    token
+                })
+            })
+        },
 }
--- routes/peduduk.router.js
+++ routes/peduduk.router.js
@@ -3,11 +3,16 @@
 const express = require("express")
 const pendudukController = require('../controllers/penduduk.controller')
 const router = new express.Router();
+const {
+    checkToken
+} = require("../auth/token_validation")
 
-router.get("/getAll", pendudukController.getAll)
-router.get("/getId/:id", pendudukController.getId)
-router.post("/add", pendudukController.add)
-router.put("/update/:id", pendudukController.update)
-router.delete("/delete/:id", pendudukController.delete)
+router.get("/getAll", checkToken, pendudukController.getAll)
+router.get("/getId/:id", checkToken, pendudukController.getId)
+router.post("/add", checkToken, pendudukController.add)
+router.put("/update/:id", checkToken, pendudukController.update)
+router.delete("/delete/:id", checkToken, pendudukController.delete)
+router.post("/registrasi", pendudukController.registrasi)
+router.get("/login", pendudukController.login)
 
-module.exports  = router
+module.exports = router
